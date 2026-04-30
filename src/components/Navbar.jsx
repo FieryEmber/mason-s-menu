226727efc2d66bf7a7827415038bbc159999b60d
FieryEmber <gekojaketheboss@gmail.com>
2026-04-30 12:18:35 -0400

    Finished Look and Contact form styling. Added restaurant photo to Home page and updated Menu page header text.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">Staff</Link>
         </li>
         <li>
           <Link to="/menu">Menu</Link>
